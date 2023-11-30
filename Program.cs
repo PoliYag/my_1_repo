9fa58e69fc95d0a458d2e30bcaa0384d01554819
PoliYag <150130450+PoliYag@users.noreply.github.com>
2023-11-30 19:54:14 +0300

    task_1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+static void Main(string[] args)
+{
+
+int[] numbers = new int[10] { 10, 21, 14, 93, 23, 5, 30, 88, 10, 50 };
+
+
+
+    int count = 0; 
+
+    Console.Write("Массив [");
+   
+    foreach (int num in numbers)
+    {
+        Console.Write(num + " ");
+        if (num >= 20 && num <= 90) 
+        {
+            count++;
+        }
+    }
+    Console.Write("] => " + count); 
+}
+}
